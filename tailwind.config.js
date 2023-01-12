be0a06e6abf427f2ecf1542e1632dfe9be36ab5f
Danny Fong <danny.fong.dev@outlook.com>
2023-01-11 22:32:53 -0800

    Custom colors and font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        strongCyan: "hsl(172, 67%, 45%)",
+
+        veryDarkCyan: "hsl(183, 100%, 15%)",
+        darkGrayishCyan: "hsl(186, 14%, 43%)",
+        grayishCyan: "hsl(184, 14%, 56%)",
+        lightGrayishCyan: "hsl(185, 41%, 84%)",
+        veryLightGrayishCyan: "hsl(189, 41%, 97%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        sans: ["Space Mono", "monospace"],
+      },
+    },
   },
   plugins: [],
-}
+};
